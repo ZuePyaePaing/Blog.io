5af8f60279c541dbb6a528a170768a2cb04ed12d
ZuePyaePaing <kaunglay24588@gmail.com>
2024-07-29 14:46:53 -0700

    password reset feature

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,6 +2,8 @@ const User = require("../models/userSchema");
 const bcrypt = require("bcrypt");
 const nodemailer = require("nodemailer");
 require("dotenv").config();
+const crypto = require("crypto");
+const { title } = require("process");
 
 //Config nodemailer
 const transpoter = nodemailer.createTransport({
@@ -100,4 +102,105 @@ exports.accountLogout = (req, res) => {
   req.session.destroy((err) => {
     res.redirect("/");
   });
+};
+
+//Render reset-password page
+
+exports.renderResetPassword = (req, res) => {
+  let message = req.flash("error");
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+  res.render("auth/resetPassword", {
+    title: "Reset Password",
+    errorMsg: message,
+  });
+};
+
+// Handle Rest Password send link
+
+exports.resetLink = (req, res) => {
+  const { email } = req.body;
+  User.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        req.flash("error", "This email dose not have an account.");
+        return res.redirect("/auth/reset-password");
+      }
+      const token = crypto.randomBytes(32).toString("hex");
+      user.token = token;
+      user.expiration = Date.now() + 180000;
+      return user.save().then((_) => {
+        res.render("auth/feekback", { title: "Password Reset" });
+        transpoter.sendMail({
+          from: process.env.SENDER_MAIL,
+          to: email,
+          subject: "Password Reset",
+          html: `<a href='http://localhost:8080/auth/reset-password/${token}'>Click the link.</a>`,
+        });
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+// render Change new password
+exports.renderChangeNewPassword = (req, res) => {
+  const { token } = req.params;
+  User.findOne({ token, expiration: { $gt: Date.now() } })
+    .then((user) => {
+      let message = req.flash(
+        "error",
+        "Password reset token is invalid or has expired."
+      );
+      if (message.length > 0) {
+        message = message[0];
+      } else {
+        message = null;
+      }
+      
+      res.render("auth/changeNewPassword", {
+        title: "Reset password",
+        errorMsg: message,
+        resetToken: token,
+        userId: user._id,
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+//handle Change new password
+exports.changeNewPassword = (req, res) => {
+  const { password, confirm_password, token, userId } = req.body;
+
+  if (password !== confirm_password) {
+    req.flash("error", "Passwords do not match.");
+    return res.redirect(`/auth/change-password/${token}`);
+  }
+
+  User.findOne({
+    token,
+    expiration: { $gt: Date.now() },
+    _id: userId,
+  })
+    .then((user) => {
+      if (!user) {
+        req.flash("error", "Password reset token is invalid or has expired.");
+        return res.redirect("/auth/reset-password");
+      }
+      return bcrypt.hash(password, 10).then((hashedPassword) => {
+        user.password = hashedPassword;
+        user.resetPasswordToken = undefined;
+        user.resetPasswordExpires = undefined;
+        return user.save();
+      });
+    })
+    .then(() => {
+      req.flash("success", "Your password has been successfully changed.");
+      res.redirect("/auth/login");
+    })
+    .catch((err) => {
+      console.error("Error during password reset:", err); 
+    });
 };
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -11,5 +11,13 @@ router.get("/login", authControllers.renderLoginPage);
 
 router.post("/login", authControllers.accountLogin);
 
-router.post('/logout', authControllers.accountLogout)
+router.post("/logout", authControllers.accountLogout);
+
+router.get("/reset-password", authControllers.renderResetPassword);
+
+router.post("/reset-password", authControllers.resetLink);
+
+router.get("/reset-password/:token", authControllers.renderChangeNewPassword);
+
+router.post("/change-password", authControllers.changeNewPassword);
 module.exports = router;
